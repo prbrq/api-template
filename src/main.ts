be30e0b92e6039640e96db712fedd5bfa551ee19
Arkady Chelyadnikov <prbrq@ya.ru>
2026-05-10 09:15:02 +0300

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('API Template')
+    .setDescription('Minimal NestJs API template')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument);
 
   const configService = app.get(ConfigService);
   const port = configService.getOrThrow<number>('PORT');
