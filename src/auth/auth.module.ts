6f07db6366b581fa96ba584a70fbc96c7a6c708e
Arkady Chelyadnikov <prbrq@ya.ru>
2026-05-10 07:33:41 +0300

    part of auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
+import { LoginUseCase } from './application/use-cases/login.use-case';
+import { PASSWORD_HASHER } from './application/ports/password-hasher';
+import { TOKEN_SERVICE } from './application/ports/token.service';
+import { BcryptPasswordHasher } from './infrastructure/services/bcrypt-password-hasher';
+import { JwtTokenService } from './infrastructure/services/jwt-token.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.getOrThrow('JWT_EXPIRES_IN'),
+        },
+      }),
+    }),
+  ],
+  providers: [
+    LoginUseCase,
+    {
+      provide: PASSWORD_HASHER,
+      useClass: BcryptPasswordHasher,
+    },
+    {
+      provide: TOKEN_SERVICE,
+      useClass: JwtTokenService,
+    },
+  ],
 })
 export class AuthModule {}
